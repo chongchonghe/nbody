6f2b32218293b4fe2689fec1b8cae1e43c1fbde0
mohammed-khalil <moh.m.khalil@gmail.com>
2017-11-11 20:43:36 -0500

    corrected leapfrog integrator

--- pp-nbody/integ_LF2.h
+++ pp-nbody/integ_LF2.h
@@ -3,19 +3,28 @@
 
 extern int DIM;  //number of dimensions
 
-void integ_LF2(float mass[], float position[][DIM], float velocity[][DIM], float force[][DIM], int N, float step)
+void integ_LF2(float mass[], float position[][DIM], float velocity[][DIM], float force[][DIM], int N, float step, float epsilon)
 {
-    //update position and velocity using leapfrog
-    float kint;
+    /*update position and velocity using leapfrog*/
+
     int i, k; //loop vars
 
+    //calculate position after half a step
+    float pos_12[N][DIM];
+    for(i = 0; i < N; i++)
+        for(k = 0; k < DIM; k++)
+            pos_12[i][k] = position[i][k] + 0.5 * step * velocity[i][k];
+
+    //force at pos_12
+    calc_force(mass, pos_12, force, N, epsilon);
+
+    //update velocity and position
     for(i = 0; i < N; i++)
     {
         for(k = 0; k < DIM; k++)
         {
-            kint = position[i][k] + 0.5 * step * velocity[i][k];
-            velocity[i][k] += step * force[i][k]/mass[i];   //divide by mass if not 1
-            position[i][k] = kint + 0.5 * step * velocity[i][k];
+            velocity[i][k] += step * force[i][k] / mass[i];
+            position[i][k] = pos_12[i][k] + 0.5 * step * velocity[i][k];
         }
     }
 }
