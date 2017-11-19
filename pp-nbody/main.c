1dad5876722ddbdcdde31ad3a448b33925cffddb
ChongChong He at Mac <che1234@umd.edu>
2017-11-19 12:39:34 -0500

    main.c

--- pp-nbody/main.c
+++ pp-nbody/main.c
@@ -1,6 +1,13 @@
-// Change log:
-// Add check_binary, is_close_encounter
-// Add a print to file at the inital step
+/*********************************************************
+ASTR615 - HW#4 Problem #2
+Particle-Particle N-body code
+
+Authors: Mohammed Khalil and ChongChong He
+
+Date: 11/18/17
+
+Filename: main.c
+*********************************************************/
 
 
 #include <stdio.h>
@@ -22,71 +29,14 @@
 
 int DIM = 3;  //number of dimensions
 
-double cal_potential(int N, DATA *data)
-{
-    double pot = 0.0;
-    double dis2;
-    for (int i = 0; i < N; i++) {
-        for (int j = i + 1; j < N; j++) {
-            dis2 = 0.0;
-            for (int k = 0; k < DIM; k++)
-                dis2 += pow((data[i].pos[k] - data[j].pos[k]), 2.0);
-            pot += - data[i].mass * data[j].mass / sqrt(dis2);
-        }
-    }
-    return pot;
-}
-
-double cal_kinetic(int N, DATA *data)
-{
-    double kin = 0.0;
-    for (int i = 0; i < N; i++)
-        for (int k = 0; k < DIM; k ++)
-            kin += data[i].mass * data[i].vel[k] * data[i].vel[k];
-    return kin / 2.0;
-}
-
-_Bool is_close_encounter(double dVirial, double mass[], double position[][DIM],
-                         double velocity[DIM])
-{
-    // check distance < alpha * dVirial
-    const double alpha = 5.0;
-    const double beta = 2.0;
-    double dist2 = 0.0;
-    
-    for (int i = 0; i < DIM; i++)
-        dist2 += (position1[i] - position2[i]) * (position1[i] - position2[i]);
-    if (dist2 > alpha * alpha * dVirial * dVirial)
-        return 0;
-
-    // if close enough to each other, check T + V < 0
-    double kinetic = 0.0;
-    for (int i = 0; i < DIM; i++)
-        kinetic += (velocity1[i] - velocity2[i]) * (velocity1[i] - velocity2[i]);
-    kinetic /= 2.0;
-    
-}
-
-//void check_binary(double mass[], double position[][DIM], double force[][DIM])
-//{
-//    int i, j, k;
-//    for (i = 0; i < N; i++) {
-//        for (j = i + 1; j < N; j++) {
-//            if is_close_encounter {
-//
-//            }
-//        }
-//    }
-//}
-
 int main(int argc, char *argv[])
 {
-    char *file_name = "binary/test01.txt";;   //name of initial conditions file
-    char *outputdir = "binary/test01";  // dir of output files
-    int N = 2;   //number of particles
-    double epsilon = 0.001;  //softening parameter
-    double t_step = 0.1;  //time step
-    int n_steps = 20;   // number of steps
+    char *file_name = "cluster03.txt";   //name of initial conditions file
+    char *outputdir = "./data/";  // dir of output files
+    int N = 1000;   //number of particles
+    double epsilon = 0.01;  //softening parameter
+    double t_step = 0.01;  //time step
+    int n_steps = 100;   // number of steps
     int op_freq = 1;    // output frequency
     char *integrator = "LF2";  //ODE integration method
     int i; //loop variable
@@ -135,9 +85,6 @@ int main(int argc, char *argv[])
 
     for(i = 0; i < n_steps; i++)
     {
-        if(i % op_freq == 0)
-            save_data(mass, position, velocity, N, (int)(i/op_freq), outputdir);
-
         calc_force(mass, position, force, N, epsilon);
 
         if( strcmp(integrator,"RK4") == 0 )
@@ -146,6 +93,9 @@ int main(int argc, char *argv[])
             integ_LF2(mass, position, velocity, force, N, t_step, epsilon);
         else
             return -1;
+
+        if(i % op_freq == 0)
+            save_data(mass, position, velocity, N, (int)(i/op_freq), outputdir);
     }
 
     return 0;
